865b88903d144ff3612e2b27982aa7eabd0439a5
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2021-09-13 15:04:34 -0700

    Require to explicitly cast environment variables

--- 04-cluster-parallel-w-dask/run_many.py
+++ 04-cluster-parallel-w-dask/run_many.py
@@ -9,7 +9,7 @@ def run_one(config_updates):
 if __name__ == '__main__':
     lazy_results = []
     gammas = [0.2, 0.5, 1., 2, 5]
-    client = dask.distributed.Client(threads_per_worker = 1, n_workers = os.environ.get('SLURM_NTASKS', len(gammas))) ## If we don't have as many cores as we have tasks this initializes a Client with the right number of cores to get the largest client we can get
+    client = dask.distributed.Client(threads_per_worker = 1, n_workers = int(os.environ.get('SLURM_NTASKS', len(gammas)))) ## If we don't have as many cores as we have tasks this initializes a Client with the right number of cores to get the largest client we can get
     for gamma in gammas:
         config_updates = {'gamma':gamma}
         lazy_results.append(dask.delayed(run_one)(config_updates))
